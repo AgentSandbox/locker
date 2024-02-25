a508e9054c9fd5c1643eba998633f4ac59cbaf09
AgentSandbox <pregrada@mail.com>
2024-02-25 14:53:48 +0100

    com4

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,24 @@ function App() {
 
                 </Scroll>
                 <Scroll html style={{width: '100%'}}>
-                    <h1> BW</h1>
-                    <h2  className='row'> SPECIAL OFFER</h2>
+                    <h1 style={{ color: '#87CEEB',position: 'absolute', top: `65vh`,left: '50%', fontSize: '4em', transform: `translate(-50%,-50%)` }}>
+                        Belgrade
+                    </h1>
+                    <h1 style={{ color: '#87CEEB',position: 'absolute', top: `75vh`,left: '50%', fontSize: '4em', transform: `translate(-50%,-50%)` }}>
+                        Woterfront
+                    </h1>
 
                     <div style={{position: 'absolute', top: `132vh`}}>
                         <div>
                             <h1>ELITE</h1>
                         </div>
                         <div className='row'>
-                            <p style={{maxWidth: '400px'}}>Featuring a sleek, metallic design inspired by advanced
-                                technology, this aftershave bottle is as stylish as it is functional. But it's not just
-                                a
-                                pretty face - inside, you'll find a nourishing and protective aftershave formula that
-                                will
-                                leave your skin feeling refreshed and hydrated.</p>
+                            <p style={{maxWidth: '400px'}}>
+                                A luxury cabinet epitomizes the epitome of opulence
+                                and refinement in furniture design. Crafted with the
+                                utmost attention to detail and using the finest materials,
+                                it stands as a statement piece in any room, exuding elegance
+                                and sophistication.</p>
 
                         </div>
 
@@ -42,29 +46,19 @@ function App() {
                             <h1>MIDDLE</h1>
                         </div>
                         <div className='row'>
-                            <p style={{maxWidth: '400px'}}>Featuring a sleek, metallic design inspired
-                                by advanced
-                                technology, this aftershave bottle is as stylish as it is functional. But it's not just
-                                a
-                                pretty face - inside, you'll find a nourishing and protective aftershave formula that
-                                will
-                                leave your skin feeling refreshed and hydrated.</p>
+                            <p style={{maxWidth: '400px'}}>A middle-class cabinet offers practicality and functionality without compromising on style. Crafted from durable materials such as engineered wood or solid hardwood veneers, it balances affordability with quality construction.
+
+                                Typically featuring a straightforward design, a middle-class cabinet focuses on practical storage solutions. It may include adjustable shelves, drawers with smooth gliding mechanisms, and simple hardware for ease of use. While it may not boast intricate carvings or luxurious finishes, it exudes a sense of warmth and homeliness that complements a variety of interior styles.</p>
                         </div>
 
                     </div>
 
                     <div className='row' style={{top: `396vh`}}>
                         <div>
-                            <h1>MINI</h1>
+                            <h1>ECONOMY</h1>
                         </div>
                         <div className='row'>
-                            <p style={{maxWidth: '400px'}}>Featuring a sleek, metallic design inspired
-                                by advanced
-                                technology, this aftershave bottle is as stylish as it is functional. But it's not just
-                                a
-                                pretty face - inside, you'll find a nourishing and protective aftershave formula that
-                                will
-                                leave your skin feeling refreshed and hydrated.</p>
+                            <p style={{maxWidth: '400px'}}>The economy class cabinet is primarily designed for those seeking a simple and budget-friendly storage solution for clothing and other items without unnecessary frills and expensive additions.</p>
                         </div>
 
                     </div>
